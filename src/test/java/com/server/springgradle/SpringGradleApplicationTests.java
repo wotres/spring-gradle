398adef0e4ad56ee5c12bba0b99182fe60bd6afb
wotres <wotres@naver.com>
2021-12-29 23:56:09 +0900

    [ADD] ì´port 수정 반영

---
+++ src/test/java/com/server/springgradle/SpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.springgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
